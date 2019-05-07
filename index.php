216361edd5201ac00814b244f9ccc205e01ea622
Angel <a.rioncervi@gmail.com>
2019-05-07 17:57:02 +0200

    added files

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="title">
+        <ul>
+            <li class="tableLi">
+                <div class="lilcirc"></div>
+            </li>
+            <li class="tableLi">
+                <h1 class="h1title">My Precious</h1>
+            </li>
+            <li class="tableLi">
+                <div class="lilcirc"></div>
+            </li>
+        </ul>
+    </div>
+    <div class="title">
+        <ul>
+            <li class="tableLi">
+                <div class="bigcirc"></div>
+            </li>
+            <li class="tableLi">
+                <h1 class="h1title">The one ring</h1>
+            </li>
+        </ul>
+    </div>
+    <div class="title">
+        <ul>
+            <li class="tableLi">
+                <h1 class="h1title">Please find it</h1>
+            </li>
+        </ul>
+    </div>
+    <div class="circles-wrap">
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+        <div class="circleLeft"></div>
+    </div>
+    <div class="circles-wrap">
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+        <div class="circleRight"></div>
+    </div>
+
+    <div class="test"></div>
+</div>
+
+</body>
+</html>
